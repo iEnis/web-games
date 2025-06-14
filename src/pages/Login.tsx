6f290a39c4dcad66d833cca1f59d6f9fe221a2bc
iEnis <enis-neimi@outlook.de>
2025-06-14 09:10:37 +0200

    fixed to use https instead of http

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,7 @@ export default function Login() {
 
     const validatePassword = async (ip: string, password: string) => {
         try {
-            const response = await fetch(`http://${ip}:54569/auth?password=${encodeURIComponent(password)}`, {
+            const response = await fetch(`https://${ip}/auth?password=${encodeURIComponent(password)}`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
